f5b8f58a28af269cafbab388c878c14b4291602a
Million Yemane <wedi_ngus@yahoo.com>
2018-04-23 13:56:17 -0500

    adding new files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+// let $=require('jquery');
+// document.getElementsByClassName(".hover").mouseout(
+//     function () {
+//         document.getElementsByClassName(".hover").removeClass("hover");
+//     }
+// );
+// document.getElementsByClassName('.hover').addEventListener('mouseleave',myfunction);
+// function myfunction(){
+//      document.getElementsByClassName(".hover").removeClass("hover");
+// };
+// document.getElementById("test").addEventListener("click", myFunction);
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "You scrolled in div.";
+// };
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//     if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+//         document.getElementById("myImg").className = "slideUp";
+// }
+
+// var blog=document.getElementById("#blog");
+// blog.onload=function () {blogs()};
+//     function blogs(){
+//         document.getElementById("#blog").innerHTML="Comming Soon";
+//     }
+// $("#blog")
+//     .mouseleave(function () {
+//         $(this).removeClass("hover");
+//     });
